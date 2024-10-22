3361860d731f28f4650a343892e2e278fc4d763e
unknown <Shorouk Yossry>
2024-10-22 17:55:09 +0300

    first commit

---
+++ task.js
@@ -0,0 +1,267 @@
+
+var pro = window.prompt("enter namber");
+console.log(pro);
+
+var m2 = +window.prompt("inter number");
+if (m2 % 2 == 0 && m2 % 3 == 0) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+
+
+/** 
+
+var m3 = window.Number(prompt("enter number one"));
+var ms3 = window.Number(prompt("enter the scond number"));
+
+if (m3 > ms3) {
+  console.log(m3);
+} else if (m3 < ms3) {
+  console.log(ms3);
+}*/
+
+
+/** 
+
+var m4 = window.Number(prompt("enter integer number"));
+
+if (m4 > 0) {
+  console.log("positive");
+} else if (m4 < 0) {
+  console.log("negative");
+}*/
+
+/** 
+
+var m5 = window.Number(prompt("enter 1 numper"));
+var ms5 = window.Number(prompt("enter 2 numper"));
+var mso5 = window.Number(prompt("enter 3 numper"));
+if (m5 > ms5 && m5 > mso5) {
+  console.log("max element is" + m5);
+} else if (ms5 > m5 && ms5 > mso5) {
+  console.log("max element is" + ms5);
+} else if (mso5 > m5 && mso5 > ms5) {
+  console.log("max element is" + mso5);
+} else if (m5 < ms5 && m5 < mso5) {
+  console.log("min element is" + m5);
+} else if (ms5 < m5 && ms5 < mso5) {
+  console.log("min element is" + ms5);
+} else if (mso5 < m5 && mso5 < ms5) {
+  console.log("min element is" + mso5);
+}*/
+/** 
+
+var m6 = window.Number(prompt("enter integer number"));
+
+if (m6 % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}*/
+/** 
+
+var m8 = window.Number(prompt("enter cracter"));
+if (m8 === a) {
+  console.log("vowei");
+} else if (m8 === e) {
+  console.log("vowei");
+} else if (m8 === o) {
+  console.log("vowei");
+} else if (m8 === u) {
+  console.log("vowei");
+} else if (m8 === i) {
+  console.log("vowei");
+} else {
+  console.log("Consonant");
+}*/
+
+/** 
+
+var m9 = window.Number(prompt("enter integer number"));
+
+for (var i = m9; i < 10; i++) {
+  console.log(i);
+}*/
+/** 
+
+var m10 = window.Number(prompt("enter number"));
+
+for (var i = m10; i <= m12; i++) {
+  console.log(i * m10);
+}*/
+/** 
+
+
+
+
+
+
+var m11 = window.Number(prompt("enter integer  even number"));
+
+
+for (var i=m11 ; i%2==0 ;i+=2){
+     console.log(i);
+}
+*/
+/** 
+
+
+
+
+var m12 = window.Number(prompt("enter integer number"));
+var me12 = window.Number(prompt("enter integer number"));
+
+console.log(m12**me12);*/
+
+
+/** 
+
+
+var a = window.Number(prompt("enter integer number"));
+var b = window.Number(prompt("enter integer  number"));
+var c = window.Number(prompt("enter integer  number"));
+var d = window.Number(prompt("enter integer  number"));
+var e = window.Number(prompt("enter integer  number"));
+
+ var f = console.log(a+b+c+d+e);
+console.log(f/2);*/
+
+
+/** 
+
+
+var m14 = window.Number(prompt("enter Physics degre"));
+var me14 = window.Number(prompt("enter Chemistry degre"));
+var mes14 = window.Number(prompt("enter Biology degre"));
+var meso14 = window.Number(prompt("enter  Mathematics degre"));
+var mesod14 = window.Number(prompt("enter Computer degre"));
+
+var n = m14*100/100;
+if (n>="90%"){
+    console.log(n +"a")
+}else if(n>="80%"){
+  console.log(n +"b")
+}else if(n>="70%"){
+  console.log(n +"c")
+}else if(n>="60%"){
+  console.log(n +"d")
+}else if(n>="50%"){
+  console.log(n +"e")
+}else if(n<="40%"){
+  console.log(n +"f")
+};*/
+
+/** 
+
+
+var month = window.prompt("enter month");
+switch(month){
+  case "janiory":
+    console.log("31 day");
+    break;
+
+    case "fabrewre":
+    console.log("28 day");
+    break;
+
+
+    case "marech":
+    console.log("30 day");
+    break;
+
+}*/
+
+/** 
+
+var check=window.prompt("inter chracter");
+
+switch(check){
+  case "a":
+    console.log("vowel");
+    break;
+
+    case "e":
+    console.log("vowel");
+    break;
+
+    case "o":
+    console.log("vowel");
+    break;
+
+    case "u":
+    console.log("vowel");
+    break;
+
+    case "i":
+    console.log("vowel");
+    break;
+    default:
+      console.log(consonant)
+}*/
+
+/** 
+var number1 = Number(prompt("inter first number"));
+var number2 = Number(prompt("enter second number"))
+
+
+switch(number1>number2){
+
+  case true:
+    console.log(" number1 is the max");
+    break;
+    case false:
+      console.log("number2 is the max");
+      break;
+
+}*/
+
+
+
+
+/**var n1 = Number(prompt("inter first number"));
+var n2 = Number(prompt("enter second number"));
+
+switch (n1%2==0) {
+  case true:
+    console.log("even")
+    
+    break;
+    case false:
+      console.log("odd");
+    break;}
+
+    switch (n2%2==0) {
+      case true:
+        console.log("even")
+        break;
+    
+      case false:
+        console.log("odd");
+        break;
+    }**/
+
+/** 
+var n = Number(prompt("enter number"));
+
+switch (n>0) {
+  case true:
+    console.log("poseteve");
+    break;
+    case false:
+      console.log("nigative");
+     break;
+  default:
+    console.log("0");
+    break;
+}*/
+
+
+
+
+
+
+
+
+
+
